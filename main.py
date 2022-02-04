344b3ead174fc40530a3bc7059e93fcd40ae1e33
gabriel <santosgabriel6763@gmail.com>
2022-02-04 11:16:21 -0200

    Ask if the player wants to continue playing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from topics import tecnologia, astronomia, geografia, curiosidades
 # Terceiros
 from random import shuffle
 import os, platform
+from time import sleep
 
 
 # Função pra limpar o buffer.
@@ -31,7 +32,7 @@ def fazer_pergunta(catg):
                     c +=1
                 else:
                     print(f'\033[1;31mQue pena você errou.\033[1;33m Resposta Correta: {catg[c][2]}\033[m')
-                    continuar = False
+                    break
         else:
             print(r'''
              ___________
@@ -70,91 +71,121 @@ def validar_alternativa():
                 print('\033[1;31mDigite uma alternativa válida.\033[m')
 
 
-limpar_buffer()
-print(r'''
-  _______________________________________
-           ___          _
-          / _ \  _   _ (_) ____
-         | | | || | | || ||_  /
-         | |_| || |_| || | / /
-          \__\_\ \__,_||_|/___|
-  ----------------------------------------
-                               \
-      Categorias                \
-
-    [0] Tecnologia                 .--.
-    [1] Astronomia                |o_o |
-    [2] Geografia                 |:_/ |
-    [3] Curiosidades             //   \ \
-    [4] Gerais                  (|     | )
-                                /'\_   _/`\
-                                \___)=(___/
-''')
-
-
-# Escolha de categoria.
+# Código Base.
 while continuar:
-    try:
-        opcao = int(input('Opção: '))
-    except KeyboardInterrupt:
-        print('\033[1;33mPrograma interrompido.\033[m')
-        continuar = False
-    except:
-        print('\033[1;31mDigite uma opção válida.\033[m')
-    else:
-        if opcao >= 0 and opcao <= 4:
-            break
-        else:
-            print('\033[1;31mDigite uma opção válida.\033[m')
-
-
-# Mostrar o título da categoria escolhida pelo jogador e chama a função principal passando a categoria como parâmetro.
-if continuar:
     limpar_buffer()
-    if opcao == 0:
-        print(r'''
-     _                             _                _
-    | |_  ___   ___  _ __    ___  | |  ___    __ _ (_)  __ _
-    | __|/ _ \ / __|| '_ \  / _ \ | | / _ \  / _` || | / _` |
-    | |_|  __/| (__ | | | || (_) || || (_) || (_| || || (_| |
-     \__|\___| \___||_| |_| \___/ |_| \___/  \__, ||_| \__,_|
-                                             |___/
-        ''')
-        fazer_pergunta(tecnologia)
-    elif opcao == 1:
-        print('''
-        _          _                                         _
-       / \    ___ | |_  _ __  ___   _ __    ___   _ __ ___  (_)  __ _
-      / _ \  / __|| __|| '__|/ _ \ | '_ \  / _ \ | '_ ` _ \ | | / _` |
-     / ___ \ \__ \| |_ | |  | (_) || | | || (_) || | | | | || || (_| |
-    /_/   \_\|___/ \__||_|   \___/ |_| |_| \___/ |_| |_| |_||_| \__,_|
-        ''')
-        fazer_pergunta(astronomia)
-    elif opcao == 2:
-        print('''
-      ____                                   __  _
-     / ___|  ___   ___    __ _  _ __  __ _  / _|(_)  __ _
-    | |  _  / _ \ / _ \  / _` || '__|/ _` || |_ | | / _` |
-    | |_| ||  __/| (_) || (_| || |  | (_| ||  _|| || (_| |
-     \____| \___| \___/  \__, ||_|   \__,_||_|  |_| \__,_|
-                         |___/
-        ''')
-        fazer_pergunta(geografia)
-    elif opcao == 3:
-        print('''
-      ____              _              _      _             _
-     / ___|_   _  _ __ (_)  ___   ___ (_)  __| |  __ _   __| |  ___  ___
-    | |   | | | || '__|| | / _ \ / __|| | / _` | / _` | / _` | / _ \/ __|
-    | |___| |_| || |   | || (_) |\__ \| || (_| || (_| || (_| ||  __/\__ \
-     \____|\__,_||_|   |_| \___/ |___/|_| \__,_| \__,_| \__,_| \___||___/
-        ''')
-        fazer_pergunta(curiosidades)
-    else:
-        print(r'''
-      ____                    _
-     / ___|  ___  _ __  __ _ (_) ___
-    | |  _  / _ \| '__|/ _` || |/ __|
-    | |_| ||  __/| |  | (_| || |\__ \
-     \____| \___||_|   \__,_||_||___/
-        ''')
-        fazer_pergunta(tecnologia+astronomia+geografia+curiosidades)
+    print(r'''
+      _______________________________________
+               ___          _
+              / _ \  _   _ (_) ____
+             | | | || | | || ||_  /
+             | |_| || |_| || | / /
+              \__\_\ \__,_||_|/___|
+      ----------------------------------------
+                                   \
+          Categorias                \
+
+        [0] Tecnologia                 .--.
+        [1] Astronomia                |o_o |
+        [2] Geografia                 |:_/ |
+        [3] Curiosidades             //   \ \
+        [4] Gerais                  (|     | )
+                                    /'\_   _/`\
+                                    \___)=(___/
+    ''')
+
+    # Escolha de categoria.
+    while True:
+        try:
+            opcao = int(input('Opção: '))
+        except KeyboardInterrupt:
+            print('\033[1;33mPrograma interrompido.\033[m')
+            continuar = False
+            break
+        except:
+            print('\033[1;31mDigite uma opção válida.\033[m')
+        else:
+            if opcao >= 0 and opcao <= 4:
+                break
+            else:
+                print('\033[1;31mDigite uma opção válida.\033[m')
+
+    # Mostrar o título da categoria escolhida pelo jogador e chama a função principal passando a categoria como parâmetro.
+    if continuar:
+        limpar_buffer()
+        if opcao == 0:
+            print(r'''
+         _                             _                _
+        | |_  ___   ___  _ __    ___  | |  ___    __ _ (_)  __ _
+        | __|/ _ \ / __|| '_ \  / _ \ | | / _ \  / _` || | / _` |
+        | |_|  __/| (__ | | | || (_) || || (_) || (_| || || (_| |
+         \__|\___| \___||_| |_| \___/ |_| \___/  \__, ||_| \__,_|
+                                                 |___/
+            ''')
+            fazer_pergunta(tecnologia)
+        elif opcao == 1:
+            print('''
+            _          _                                         _
+           / \    ___ | |_  _ __  ___   _ __    ___   _ __ ___  (_)  __ _
+          / _ \  / __|| __|| '__|/ _ \ | '_ \  / _ \ | '_ ` _ \ | | / _` |
+         / ___ \ \__ \| |_ | |  | (_) || | | || (_) || | | | | || || (_| |
+        /_/   \_\|___/ \__||_|   \___/ |_| |_| \___/ |_| |_| |_||_| \__,_|
+            ''')
+            fazer_pergunta(astronomia)
+        elif opcao == 2:
+            print('''
+          ____                                   __  _
+         / ___|  ___   ___    __ _  _ __  __ _  / _|(_)  __ _
+        | |  _  / _ \ / _ \  / _` || '__|/ _` || |_ | | / _` |
+        | |_| ||  __/| (_) || (_| || |  | (_| ||  _|| || (_| |
+         \____| \___| \___/  \__, ||_|   \__,_||_|  |_| \__,_|
+                             |___/
+            ''')
+            fazer_pergunta(geografia)
+        elif opcao == 3:
+            print('''
+          ____              _              _      _             _
+         / ___|_   _  _ __ (_)  ___   ___ (_)  __| |  __ _   __| |  ___  ___
+        | |   | | | || '__|| | / _ \ / __|| | / _` | / _` | / _` | / _ \/ __|
+        | |___| |_| || |   | || (_) |\__ \| || (_| || (_| || (_| ||  __/\__ \
+         \____|\__,_||_|   |_| \___/ |___/|_| \__,_| \__,_| \__,_| \___||___/
+            ''')
+            fazer_pergunta(curiosidades)
+        else:
+            print(r'''
+          ____                    _
+         / ___|  ___  _ __  __ _ (_) ___
+        | |  _  / _ \| '__|/ _` || |/ __|
+        | |_| ||  __/| |  | (_| || |\__ \
+         \____| \___||_|   \__,_||_||___/
+            ''')
+            fazer_pergunta(tecnologia+astronomia+geografia+curiosidades)
+
+    # Pergunta se o jogador deseja continuar jogando.
+    if continuar:
+        print()
+        sleep(2)
+        limpar_buffer()
+        print('='*30)
+        while True:
+            try:
+                escolha = str(input('Deseja ir um novo jogo? [S/N] ')).upper().strip()[0]
+            except KeyboardInterrupt:
+                print('\033[1;33mPrograma interrompido.\033[m')
+                continuar = False
+                break
+            except:
+                print('\033[1;31mDigite uma alternativa válida.\033[m')
+            else:
+                if escolha in 'SN':
+                    if escolha == 'N':
+                        continuar = False
+                        print('\033[1;34mEncerrando...\033[m')
+                        sleep(2)
+                    else:
+                        print('\033[1;34mCarregando...\033[m')
+                        sleep(2)
+                    break
+                else:
+                    print('\033[1;31mDigite uma alternativa válida.\033[m')
+
